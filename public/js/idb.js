dce43e6638bc9cda5b5c7d8aff461a42279e5c47
Kyle <kylejmbaylis@gmail.com>
2022-06-22 17:30:33 -0400

    working on idb file

---
+++ public/js/idb.js
@@ -0,0 +1,55 @@
+// variable to hold db connection
+let db;
+
+// establish connection to IndexedDB database, set to version 1
+const request = indexedDB.open('budget_tracker', 1);
+
+// event for if database changes
+request.onupgradeneeded = function(event) {
+    // save reference to db
+    const db = event.target.result;
+
+    // creates object store and auto increments
+    db.createObjectStore('new_entry', {autoIncrement: true});
+};
+
+// successful
+request.onsuccess = function(event) {
+    // when db is created successfully with object store or connection established, save a reference
+    db = event.target.result;
+
+    // if app is online, send local data to api
+    if(navigator.onLine) {
+        //uploadData function here
+        //
+
+    }
+};
+
+// if error
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+// function in case of new upload with no internet connection
+function saveRecord(transaction) {
+    // open new transaction with database - includes read and write permissions
+    const transaction = db.transaction(['new_entry'], 'readwrite');
+
+    // access object store of 'new_entry'
+    const entryObjectStore = transaction.objectStore('new_entry');
+
+    // add record to store
+    entryObjectStore.add(transaction);
+};
+
+function uploadRecord() {
+    // open transaction in db
+    const transaction = db.transaction(['new_entry'], 'readwrite');
+
+    // access object store
+    const entryObjectStore = transaction.objectStore('new_entry');
+
+    // get all records from store and set to variable
+    const getAll = entryObjectStore.getAll();
+};
